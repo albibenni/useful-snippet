12495798cce96b42b46bf569bb46a361f3186ee4
Alberto Benatti <albi.benni8@gmail.com>
2024-08-22 11:11:19 +0200

    events

--- events/events-delegation.ts
+++ events/events-delegation.ts
@@ -1,5 +1,5 @@
-function addGlobalEventListener<K>(type: keyof DocumentEventMap, selector: string, callback: (ev: Event) => K) {
-    document.addEventListener(type, e => {
+function addGlobalEventListener<K, E extends HTMLElement>(type: keyof DocumentEventMap, selector: string, callback: (ev: Event) => K, parentElement: Document | E = document) {
+    parentElement.addEventListener(type, (e: Event) => {
         if (e.target instanceof Element) {
             if (e.target.matches(selector)) callback(e);
         }
